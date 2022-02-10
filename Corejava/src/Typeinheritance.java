9bfea48ebe02778f11bd23f29088dda2d9710f88
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-10 08:30:10 +0530

    4 th commit

---
+++ Corejava/src/Typeinheritance.java
@@ -0,0 +1,22 @@
+
+class parent1{
+	void sum() {
+		
+	}
+}
+class parent2{
+}
+class parent3 extends parent2{
+	void sum() {
+		
+	}
+}
+
+public class Typeinheritance extends parent1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
