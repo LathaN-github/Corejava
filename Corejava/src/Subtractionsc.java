898f7c738fde678980a076c1d03dccd0807d25b2
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-15 13:13:03 +0530

    commit

---
+++ Corejava/src/Subtractionsc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Subtractionsc {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter a 1st value: ");
+	      int a = sc.nextInt();
+	      System.out.println("Enter a 2nd value: ");
+	      int b = sc.nextInt();
+			    int result =a-b;
+			    System.out.println("the  Subtraction of a and b is :" + result);
+
+			}
+
+	}
+
+
