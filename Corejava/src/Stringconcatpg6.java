aa38ac873407bf5f57922d6e977eacd1d4dd3298
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-22 22:03:46 +0530

    commit

---
+++ Corejava/src/Stringconcatpg6.java
@@ -0,0 +1,14 @@
+
+public class Stringconcatpg6 {
+
+	public static void main(String[] args) {
+		
+			   String s="Latha";  
+			   s=s.concat(" Nagaraj ");  
+			   System.out.println(s);  
+			 }  
+			  // TODO Auto-generated method stub
+
+	}
+
+
---
+++ Corejava/src/Stringequalpg2.java
@@ -0,0 +1,12 @@
+
+class Stringequalpg2{  
+ public static void main(String args[]){  
+   String s1="Latha";  
+   String s2="Latha";  
+   String s3=new String("Latha");  
+   String s4="Nagaraj";  
+   System.out.println(s1.equals(s2));
+   System.out.println(s1.equals(s3));  
+   System.out.println(s1.equals(s4));  
+ }  
+}  
