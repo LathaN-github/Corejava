3f3ce6d306f9816c860d84a04ef78552576ab5b8
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-22 11:06:21 +0530

    commit

---
+++ Corejava/src/Array1Ddemo.java
@@ -0,0 +1,19 @@
+
+public class Array1Ddemo {
+
+	public static void main(String[] args) {
+		int[] a=new int[3];//declaration   
+		a[0]=10;//initialization  
+		a[1]=20;  
+		a[2]=30;  
+		//printing array  
+		System.out.println("One dimensional array elements are");    
+		System.out.println(a[0]);    
+		System.out.println(a[1]);    
+		System.out.println(a[2]);    
+		}
+	// TODO Auto-generated method stub
+
+	}
+
+
