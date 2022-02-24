afed67577b5aa7bd7d9134cd69dc0aa98b91176c
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-24 23:01:45 +0530

    commit

---
+++ Arraylistpg1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Arraylistpg1 {
+
+	public static void main(String[] args) {
+		 
+		  ArrayList<String> list=new ArrayList<String>();    
+		      list.add("latha");//    
+		      list.add("harshi");    
+		      list.add("yogi");    
+		      list.add("charvi");    
+		        
+		      System.out.println(list);  
+		 }  
+		
+
+	}
+
+
