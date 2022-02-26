4f97a87f63ca00a4a7dc1708e3dc6c62531f614d
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-26 13:13:46 +0530

    commit

---
+++ Corejava/src/Enumpg.java
@@ -0,0 +1,21 @@
+package corejava;
+
+public class Enumpg {
+	public enum Season { WINTER, SPRING, SUMMER, FALL }  
+
+	public static void main(String[] args) {
+		
+			 
+			//printing all enum  
+			for (Season s : Season.values()){  
+			System.out.println(s);  
+			}  
+			System.out.println("Value of WINTER is: "+Season.valueOf("WINTER"));  
+			System.out.println("Index of WINTER is: "+Season.valueOf("WINTER").ordinal());  
+			System.out.println("Index of SUMMER is: "+Season.valueOf("SUMMER").ordinal());  
+			  
+			
+
+	}
+
+}
