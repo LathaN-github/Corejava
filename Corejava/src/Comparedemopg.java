7812d383639612b4b409c1a3c3ed665e7041af25
LathaN-github <98800529+LathaN-github@users.noreply.github.com>
2022-02-28 19:20:08 +0530

    commit

---
+++ Corejava/src/Comparedemopg.java
@@ -0,0 +1,20 @@
+package corejava;
+
+public class Comparedemopg {
+
+	public static void main(String[] args) {
+	
+			
+			String s1="Latha";
+			String s2="Nagaraj";
+			System.out.println("result:"+s2.compareTo(s1));
+			
+
+			}
+
+		
+
+}    
+
+
+
